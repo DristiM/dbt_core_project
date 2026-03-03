dd4996d58fc11a5a8892c67a773840c8e64003a2
Dristi Manandhar <dristi87@gmail.com>
2026-03-03 11:18:45 +0000

    added stripe payments staging model

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,8 @@
+select
+  "ID" as payment_id,
+  "ORDERID" as order_id,
+  "PAYMENTMETHOD" as payment_method,
+  "STATUS" as payment_status,
+  "AMOUNT" as amount,
+  "CREATED" as created_at
+from {{ source('stripe', 'payments') }}
