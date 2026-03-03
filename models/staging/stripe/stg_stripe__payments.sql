3bee78d1daddaf6ed3fe7def5e53f743f25954e6
Dristi Manandhar <dristi87@gmail.com>
2026-03-03 13:45:51 +0000

    renamed columns in stripe payments stg

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,13 @@
+with
+    source as (
+        select * from {{ source('stripe', 'payments') }}
+    )
+
 select
   "ID" as payment_id,
   "ORDERID" as order_id,
   "PAYMENTMETHOD" as payment_method,
   "STATUS" as payment_status,
-  "AMOUNT" as amount,
-  "CREATED" as created_at
-from {{ source('stripe', 'payments') }}
+  "AMOUNT" as payment_amount,
+  "CREATED" as payment_created
+from source
